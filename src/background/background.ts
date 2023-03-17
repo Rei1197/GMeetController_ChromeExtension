c825b3c70b821f86462beae908e69a025ec9bbb5
Rei <reikamiya2016@gmail.com>
2023-03-17 10:39:30 +0900

    Final

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,30 +1,153 @@
 console.log('this is working from background.ts')
 
 // import * as tf from '@tensorflow/tfjs';
+// import * as cocoSsd from '@tensorflow-models/coco-ssd';
 // import '@tensorflow/tfjs-backend-cpu';
 // import '@tensorflow/tfjs-backend-webgl';
-// import * as tf from "@tensorflow/tfjs";
-// import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
-// export async function loadModel(): Promise<tf.GraphModel> {
-//   const model = await tf.loadGraphModel('https://tfhub.dev/tensorflow/tfjs-model/coco-ssd/1/default/1');
-//   console.log('Model loaded.');
-//   return model;
+// let model: cocoSsd.ObjectDetection;
+
+// const loadModel = async () => {
+//   console.log("Loading TensorFlow and coco-ssd model...");
+//   await tf.ready();
+//   model = await cocoSsd.load();
+//   console.log("Model loaded successfully!");
+// };
+
+// const cocossd = require('@tensorflow-models/coco-ssd');
+// if (!cocossd) {
+//           console.error();
+//           console.log("fail to load model");
+//       }else 
+//       {
+//         console.log("the model is loaded", cocossd)
+//       }
+
+chrome.runtime.onInstalled.addListener(() => {
+  // loadModel();
+
+  // Create the context menu
+  chrome.contextMenus.create({
+    id: "startDetecting",
+    title: "Start Detection on Google",
+    contexts: ["all"],
+  });
+
+  chrome.contextMenus.create({
+    id: "stopDetecting",
+    title: "Stop Detection on Google",
+    contexts: ["all"],
+  });
+
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === 'startDetecting') {
+    chrome.tabs.sendMessage(tab.id, { type: 'startDetecting' });
+  } else if (info.menuItemId === 'stopDetecting') {
+    chrome.tabs.sendMessage(tab.id, { type: 'stopDetecting' });
+  }
+});
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "processFrame") {
+//     const { frameData } = message;
+//     const img = new Image();
+//     const imageURL = URL.createObjectURL(frameData);
+
+//     img.src = imageURL;
+
+//     img.onload = async () => {
+//       const predictions = await model.detect(img);
+//       console.log("Predictions:", predictions);
+//       URL.revokeObjectURL(imageURL);
+//       sendResponse(predictions);
+//     };
+
+//     return true; // Indicates that the response will be sent asynchronously
+//   }
+// });
+
+
+
+
+// // Types for Chrome Manifest V3
+// type Message = {
+//   type: string;
+//   tabId?: number;
+// };
+
+// // Listen for installation and create a context menu item
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.contextMenus.create({
+//     id: "logMeetVideoStream",
+//     title: "Log Google Meet Video",
+//     contexts: ["page"],
+//     documentUrlPatterns: ["*://meet.google.com/*"]
+//   })
+// });
+
+
+// // Handle context menu click
+// chrome.contextMenus.onClicked.addListener((info, tab) => {
+//   if (info.menuItemId === "logMeetVideoStream" && tab) {
+//     logMeetVideoStream(tab.id);
+//   }
+// });
+
+// Handle context menu click
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "logMeetVideoStream" && tab) {
+    chrome.tabs.sendMessage(tab.id, { type: "logMeetVideoStream"});
+  }
+});
+
+// async function clickMenuCallback() {
+
+//         chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+//             const constraints = {
+//                 video: true
+//               };
+            
+              
+//               const video = document.createElement('video');
+//               navigator.mediaDevices.getUserMedia(constraints).
+//                 then((stream) => {video.srcObject = stream});
+//                 video.play();
+        
+          
+//             // Classify the image.
+//             const predictions = await cocossd.detect(video);
+          
+//             console.log('Predictions: ');
+//             console.log(predictions);
+//         });
+// };
+
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.contextMenus.create({
+//       id: 'contextMenu0',
+//       title: 'detect objects with TensorFlow.js ',
+//       contexts: ['page'],
+//     });
+//   });
+  
+//   chrome.contextMenus.onClicked.addListener(clickMenuCallback);
+  
+
+// async function loadedModel(){
+//     try {
+//         const model = await cocoSsd.load();
+//         console.log("model is loaded", model);
+//     }catch (err){
+//         console.log(err)
+//         console.log("fail to load model")
+//     }
 // }
-  
-//   self.addEventListener("install", (event: Event) => {
-//     const extendableEvent = event as ExtendableEvent;
-//     extendableEvent.waitUntil(
-//       loadModel().then(([model, cocoModel]) => {
-//         self["model"] = model;
-//         self["cocoModel"] = cocoModel;
-//       })
-//     );
-//   });
-  
-//   self.addEventListener("fetch", (event: FetchEvent) => {
-//     event.respondWith(fetch(event.request));
-//   });
+
+// loadedModel()
+
+
 
 //   function openCam(){
 //   const constraints = {
--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -2,81 +2,315 @@ window.onload= () => {
 	console.log("I am from content Script.");
 }
 
-
-import '@tensorflow/tfjs-backend-cpu';
-import '@tensorflow/tfjs-backend-webgl';
 import * as tf from "@tensorflow/tfjs";
-import * as cocoSsd from '@tensorflow-models/coco-ssd';
-
-
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
 
 let isDetecting = false;
-let videoStream: MediaStream;
-let detectObjectsTimeoutId: number;
-let videoElement: HTMLVideoElement | null = null;
-let canvasElement: HTMLCanvasElement | null = null;
-let ctx: CanvasRenderingContext2D | null = null;
-let model: cocoSsd.ObjectDetection | null = null;
 
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "startDetection") {
-    startDetection();
-  } else if (message.type === "stopDetection") {
-    stopDetection();
-  } else if (message.type === "getStatus") {
-    sendResponse({ isDetecting: isDetecting });
-  }
-});
-export async function loadModel(): Promise<tf.GraphModel> {
-  const model = await tf.loadGraphModel('https://tfhub.dev/tensorflow/tfjs-model/coco-ssd/1/default/1');
-  console.log('Model loaded.');
-  return model;
-}
-
-async function startDetection(): Promise<void> {
-
-  isDetecting = true;
-  chrome.runtime.sendMessage({ type: "statusChange", isDetecting: isDetecting });
-
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  if (!stream) {
-    console.error('Could not get video stream.');
-    return;
-  }
+chrome.runtime.onMessage.addListener(async (message: any) => {
+	if (message.type === 'startDetecting') {
+		isDetecting = true;
+	  	logMeetVideoStream();
+	} else if (message.type === 'stopDetecting') {
+		isDetecting = false;
+		if (overlayCanvas) {
+			clearCanvas(overlayCanvas);
+		  }
+	}
+  });
   
-  const video = document.createElement("video");
-  video.srcObject = stream;
-  video.play();
+  const captureVideoFrame = async (videoElement: HTMLVideoElement) => {
+	const offscreenCanvas = new OffscreenCanvas(videoElement.videoWidth, videoElement.videoHeight);
+	const ctx = offscreenCanvas.getContext("2d");
+	ctx.drawImage(videoElement, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+	return ctx.getImageData(0, 0, offscreenCanvas.width, offscreenCanvas.height);
+  };
 
-  const model = await loadModel()
-  const cocoModel = await cocoSsd.load()
 
-  async function detectObjects() {
-    const image = tf.browser.fromPixels(video);
-    const predictions = await cocoModel.detect(image);
+  const findVideoElements = () => {
+	const containerElements = document.getElementsByClassName("p2hjYe TPpRNe");
+	const videoElements: HTMLVideoElement[] = [];
+  
+	for (const container of Array.from(containerElements)) {
+	  const videos = container.getElementsByTagName("video");
+	  if (videos.length > 0) {
+		videoElements.push(videos[0]);
+	  }
+	}
+	return videoElements;
+  };
 
-    if (predictions.length > 0) {
-      console.log("Predictions:", predictions);
-    }
-    image.dispose();
-    if (isDetecting) {
-      requestAnimationFrame(detectObjects);
-    }
-  }
-  requestAnimationFrame(detectObjects);
-}
+  
+  const logMeetVideoStream = async () => {
+	const videos = findVideoElements();
+	console.log(`Number of video elements: ${videos.length}`);
+  
+	if (videos.length === 0) {
+	  console.error('No video elements found on the page.');
+	  return;
+	}
+  
+	const video = videos[0];
 
-function stopDetection() {
-  isDetecting = false;
-  clearTimeout(detectObjectsTimeoutId);
-  chrome.runtime.sendMessage({ type: "statusChange", isDetecting: isDetecting });
+	const createCanvasOverlay = (videoElement: HTMLVideoElement) => {
+		const canvas = document.createElement('canvas');
+		canvas.width = video.clientWidth;
+		canvas.height = video.clientHeight;
+		canvas.style.position = "absolute";
+		canvas.style.top = "0";
+		canvas.style.left = "0";
+		// canvas.style.zIndex = '999';
+	
+		const parentElement = videoElement.parentElement;
+		parentElement.style.position = 'relative';
+		parentElement.appendChild(canvas);
+	
+		return canvas;
+	  };
 
-  if (videoStream) {
-    videoStream.getTracks().forEach((track) => track.stop());
-    videoStream = null;
-  }
-}
+	const canvas = createCanvasOverlay(video);
+  
+	console.log('Loading model...');
+	await tf.ready();
+	const model = await cocoSsd.load();
+	console.log('Model loaded successfully.');
+
+
+	const drawBoundingBoxes = (canvas: HTMLCanvasElement, predictions: cocoSsd.DetectedObject[]) => {
+	  const ctx = canvas.getContext('2d');
+	  ctx.clearRect(0, 0, canvas.width, canvas.height);
+	  ctx.lineWidth = 2;
+  
+	  predictions.forEach(({ bbox, class: detectedClass }) => {
+		const [x, y, width, height] = bbox;
+		ctx.strokeStyle = 'red';
+		ctx.strokeRect(x, y, width, height);
+		ctx.font = '14px Arial';
+		ctx.fillStyle = 'red';
+		ctx.fillText(detectedClass, x, y - 4);
+	  });
+	};
+  
+	// const captureVideoFrameAndDrawBoundingBoxes = async (
+	//   videoElement: HTMLVideoElement,
+	//   predictions: cocoSsd.DetectedObject[]
+	// ) => {
+	//   const offscreenCanvas = new OffscreenCanvas(videoElement.videoWidth, videoElement.videoHeight);
+	//   const ctx = offscreenCanvas.getContext("2d");
+	//   ctx.drawImage(videoElement, 0, 0, offscreenCanvas.width, offscreenCanvas.height);
+  
+	//   drawBoundingBoxes(offscreenCanvas, predictions);
+  
+	//   return offscreenCanvas;
+	// };
+  
+	const processFrame = async () => {
+		if(!isDetecting){
+			return
+		}
+	//   if (video.readyState === video.HAVE_ENOUGH_DATA) {
+		// console.log("Processing frame...");
+  
+		// console.log("Detecting objects in frame...");
+		const frameData = await captureVideoFrame(video);
+		const predictions = await model.detect(frameData);
+  
+		const formattedPredictions = predictions.map(({ class: detectedClass, score }) => ({
+		  detectedClass,
+		  score,
+		}));
+		console.log("Predictions:", formattedPredictions);
+		drawBoundingBoxes(canvas, predictions);
+  
+		// // Draw bounding boxes on the canvas
+		// const offscreenCanvasWithBoundingBoxes = await captureVideoFrameAndDrawBoundingBoxes(
+		//   video,
+		//   predictions
+		// );
+	//   }
+  
+	  requestAnimationFrame(processFrame);
+	};
+  
+	processFrame();
+  };
+  
+  const clearCanvas = (canvas: HTMLCanvasElement) => {
+	const ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+  
+  let overlayCanvas: HTMLCanvasElement;
+
+//   const logGoogleMeetVideoFrames = () => {
+// 	console.log("logGoogleMeetVideoFrames called");
+  
+// 	const captureVideoFrame = async (video: HTMLVideoElement): Promise<Blob | null> => {
+// 		const canvas = document.createElement("canvas");
+// 		canvas.width = video.videoWidth;
+// 		canvas.height = video.videoHeight;
+// 		const ctx = canvas.getContext("2d");
+// 		ctx!.drawImage(video, 0, 0, canvas.width, canvas.height);
+// 		return new Promise((resolve) => canvas.toBlob(resolve, "image/jpeg"));
+// 	  };
+	  
+	
+  
+// 	const videos = findVideoElements();
+// 	console.log("Number of video elements:", videos.length);
+  
+// 	for (const video of videos) {
+// 	  processVideoElement(video);
+// 	}
+//   };
+
+//   chrome.runtime.onMessage.addListener((message: Message, sender, sendResponse) => {
+// 	console.log('Message received in content script:', message);
+// 	if (message.type === "logMeetVideoStream") {
+// 	  logGoogleMeetVideoFrames();
+// 	}
+//   });
+  
+
+
+// const logMeetVideoStream = async (tabId: number) => {
+// 	try {
+// 	  const constraints: chrome.tabCapture.CaptureOptions = {
+// 		audio: false,
+// 		video: true,
+// 		videoConstraints: {
+// 		  mandatory: {
+// 			minWidth: 1280,
+// 			minHeight: 720,
+// 			minFrameRate: 30,
+// 		  },
+// 		},
+// 	  };
+  
+  
+// 	  if (tabId) {
+// 		chrome.tabCapture.capture(constraints, (stream: MediaStream) => {
+// 		  if (chrome.runtime.lastError) {
+// 			console.error("Error capturing stream:", chrome.runtime.lastError);
+// 			return;
+// 		  }
+// 		  console.log("Google Meet Video Stream:", stream);
+// 		});
+// 	  }
+// 	} catch (error) {
+// 	  console.error("Error logging Google Meet video stream:", error);
+// 	}
+//   };
+  
+//   chrome.runtime.onMessage.addListener((message: Message, sender, sendResponse) => {
+// 	if (message.type === "logMeetVideoStream" && message.tabId ) {
+// 	  logMeetVideoStream(message.tabId);
+// 	}
+//   });
+
+// const constraints = {
+// 	video: true,
+//   };
+  
+//   navigator.mediaDevices
+// 	.getUserMedia(constraints)
+// 	.then((mediaStream) => {
+// 	  const video = document.createElement("video");
+// 	  video.srcObject = mediaStream;
+// 	  console.log("video tracks", video.srcObject)
+// 	  video.onloadedmetadata = () => {
+// 		video.play();
+// 	  };
+// 	})
+// 	.catch((err) => {
+// 	  // always check for errors at the end.
+// 	  console.error(`${err.name}: ${err.message}`);
+// 	});
+
+// const script = document.createElement("script");
+// script.setAttribute("type", "module");
+// script.setAttribute("src", chrome.runtime.getURL("background.ts"));
+
+// import * as tf from "@tensorflow/tfjs";
+// import * as cocoSsd from '@tensorflow-models/coco-ssd';
+// import '@tensorflow/tfjs-backend-cpu';
+// import '@tensorflow/tfjs-backend-webgl';
+
+
+
+// let isDetecting = false;
+// let videoStream: MediaStream;
+// let detectObjectsTimeoutId: number;
+// let videoElement: HTMLVideoElement | null = null;
+// let canvasElement: HTMLCanvasElement | null = null;
+// let ctx: CanvasRenderingContext2D | null = null;
+// let model: cocoSsd.ObjectDetection | null = null;
+
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "startDetection") {
+//     startDetection();
+//   } else if (message.type === "stopDetection") {
+//     stopDetection();
+//   } else if (message.type === "getStatus") {
+//     sendResponse({ isDetecting: isDetecting });
+//   }
+// });
+
+// const cocoModel = cocoSsd.load();
+//   if (!cocoModel) {
+//       console.error();
+//       console.log("fail to load model");
+//   }else 
+//   {
+//     console.log("the model is loaded", cocoModel)
+//   }
+
+
+// async function startDetection(): Promise<void> {
+
+//   isDetecting = true;
+//   chrome.runtime.sendMessage({ type: "statusChange", isDetecting: isDetecting });
+
+//   const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+//   if (!stream) {
+//     console.error('Could not get video stream.');
+//     return;
+//   }
+//   console.log(stream)
+  
+//   const video = document.createElement("video");
+//   video.srcObject = stream;
+//   video.play();
+
+
+//   async function detectObjects() {
+//     const image = tf.browser.fromPixels(video);
+//     const predictions = await (await cocoModel).detect(image);
+
+//     if (predictions.length > 0) {
+//       console.log("Predictions:", predictions);
+//     }
+//     image.dispose();
+//     if (isDetecting) {
+//       requestAnimationFrame(detectObjects);
+//     }
+//   }
+//   requestAnimationFrame(detectObjects);
+// }
+
+// startDetection();
+
+// function stopDetection() {
+//   isDetecting = false;
+//   clearTimeout(detectObjectsTimeoutId);
+//   chrome.runtime.sendMessage({ type: "statusChange", isDetecting: isDetecting });
+
+//   if (videoStream) {
+//     videoStream.getTracks().forEach((track) => track.stop());
+//     videoStream = null;
+//   }
+// }
 
 // const startCamera = (sendResponse: (response: { streamId: string }) => void) => {
 //   console.log("the camera is clicked!");

bdabb685dd32cd33564b89debdd6c3c507c68764
Rei-kun <reikamiya2016@gmail.com>
2023-08-21 20:33:22 +0900

    output done

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,24 +1,24 @@
 import * as tf from "@tensorflow/tfjs";
 import { loadGraphModel } from "@tensorflow/tfjs-converter";
 
-// let isDetecting = false;
+let isDetecting = false;
 let customModelURL = chrome.runtime.getURL('best_web_model.json')
 let model = null;
 
 chrome.runtime.onMessage.addListener(async (message: any, sender, sendResponse) => {
-	if (message.type === 'startDetecting') {
-	//   isDetecting = true;
-	  await loadCustomModel();
+  if (message.type === 'startDetecting') {
+        isDetecting = true;
+    await loadCustomModel();
     if (model) {
       detectObjectOnMeet();
     }
-	}
-  });
+  }
+});
+
 
 const findVideoElements = () => {
   const meetView = document.getElementsByClassName("p2hjYe TPpRNe");
-  const videoElements : HTMLVideoElement [] = [];
-
+  const videoElements: HTMLVideoElement[] = [];
   for (const container of Array.from(meetView)) {
     const videos = container.getElementsByTagName("video");
     if (videos.length > 0) {
@@ -35,7 +35,7 @@ const loadCustomModel = async () => {
   try {
     // customModel = await tf.loadLayersModel(chrome.runtime.getURL('model.json')); 
     await tf.setBackend('webgl');
-    if (tf.ready()){
+    if (tf.ready()) {
       model = await loadGraphModel(customModelURL)
       console.log('Model loaded successfully.');
       return true;
@@ -43,7 +43,7 @@ const loadCustomModel = async () => {
       console.error('TensorFlow.js backend is not ready.');
       return false;
     }
-    
+
   } catch (error) {
     console.error('Failed to load the model:', error);
     return false;
@@ -57,74 +57,54 @@ const detectObjectOnMeet = async () => {
         let tensor = tf.browser.fromPixels(video);
         tensor = tensor.resizeBilinear([640, 640]);
         tensor = tensor.expandDims(0);
-        try{
-          const results = await model.predict(tensor);
-          const resultsArray = await results.array();
-          console.log(resultsArray);  // Let's log to inspect the output structure
+        const results = await model.predict(tensor);
+        const resultsArray = await results.array();
+        console.log(resultsArray)
 
-          if (!resultsArray || resultsArray.length < 3) {
-            console.error('Unexpected results array structure:', resultsArray);
-            return;
-          }
+        const boxes = resultsArray[0][0][0];
+        // const scores = resultsArray[1][0] || [];  // Provide a default value if index doesn't exist
+        // const classes = resultsArray[2][0] || [];
 
-          const boxes = resultsArray[0];
-          const scores = resultsArray[1][0] || [];  // Provide a default value if index doesn't exist
-          const classes = resultsArray[2][0] || [];
+        //console.log(boxes)
+        // console.log(scores)
+        // console.log(classes)
+        
+        // Log and visualize bounding boxes
+        // visualizeResults(video, resultsArray);
 
-          if (boxes.length !== scores.length || boxes.length !== classes.length) {
-            console.error('Mismatched dimensions between boxes, scores, and classes.');
-            return;
-          }
-
-          // Create a results array to match what visualizeResults expects
-          const detections = boxes.map((box, index) => {
-            return {
-              y1: box[0],
-              x1: box[1],
-              y2: box[2],
-              x2: box[3],
-              confidence: scores[index],
-              label: classes[index] === 1 ? 'person' : 'other'
-            };
-          });
-
-          visualizeResults(video, detections);
-
-        } catch (error) {
-          console.error('error in detection:', error);
-        }
+        // Clean up tensor to prevent GPU memory leak
         tensor.dispose();
     }
   }
   setTimeout(detectObjectOnMeet, 1000);
 }
 
-const visualizeResults = (video: HTMLVideoElement, detections: any[]) => {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+// const visualizeResults = (video: HTMLVideoElement, resultsArray: any[]) => {
+//   const canvas = document.createElement('canvas');
+//   const ctx = canvas.getContext('2d');
 
-  canvas.width = video.clientWidth;
-  canvas.height = video.clientHeight;
-  canvas.style.position = 'absolute';
-  canvas.style.top = `${video.offsetTop}px`;
-  canvas.style.left = `${video.offsetLeft}px`;
-  canvas.style.pointerEvents = 'none';
+//   canvas.width = video.clientWidth;
+//   canvas.height = video.clientHeight;
+//   canvas.style.position = 'absolute';
+//   canvas.style.top = `${video.offsetTop}px`;
+//   canvas.style.left = `${video.offsetLeft}px`;
+//   canvas.style.pointerEvents = 'none';
 
-  video.parentElement?.appendChild(canvas);
+//   video.parentElement?.appendChild(canvas);
 
-  for (const detection of detections) {
-    const { x1, y1, x2, y2, confidence, label } = detection;
+//   for (const result of resultsArray) {
+//     const { x, y, width, height, confidence, label } = result; // Modify based on your model's output structure
+    
+//     if (confidence > 0.9) {
+//       ctx!.strokeStyle = label === 'person' ? 'red' : 'blue';
+//       ctx!.lineWidth = 2;
+//       ctx!.strokeRect(x, y, width, height);
 
-    if (confidence > 0.5) { // You can adjust this threshold as required
-      ctx!.strokeStyle = label === 'person' ? 'red' : 'blue';
-      ctx!.lineWidth = 2;
-      ctx!.strokeRect(x1, y1, x2 - x1, y2 - y1);  // Adjusting for the actual width and height of the box
-
-      if (label === 'person') {
-        console.log('Detected a person with confidence:', confidence);
-      } else {
-        console.log('Detected others with confidence:', confidence);
-      }
-    }
-  }
-}
+//       if (label === 'person') {
+//         console.log('Detected a person with confidence:', confidence);
+//       } else {
+//         console.log('Detected others with confidence:', confidence);
+//       }
+//     }
+//   }
+// }

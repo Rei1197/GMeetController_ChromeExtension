1b4ea7b2f43cd26b2e6e3ad46daba87723575c77
Rei-kun <reikamiya2016@gmail.com>
2023-09-01 20:33:23 +0900

    add postprocess function

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,5 +1,30 @@
 import * as tf from "@tensorflow/tfjs";
 import { loadGraphModel } from "@tensorflow/tfjs-converter";
 
+let isDetecting = false
 let customModelURL = chrome.runtime.getURL('best_web_model.json')
+
+chrome.runtime.onMessage.addListener(async (message: any, sender, sendResponse) => {
+    if (message.type === 'startDetecting') {
+      isDetecting = true;
+      await loadModel();
+      if (customModelURL) {
+        detection();
+      }
+    }
+    else if (message.type === 'stopDetecting') {
+        isDetecting = false;
+        const videoElements = findVideo();
+		for (const video of videoElements) {
+			const canvas = video.parentElement.querySelector('canvas');
+			if (canvas){
+				clearCanvas(canvas);
+			}
+      }
+  }});
+
+  const clearCanvas = (canvas: HTMLCanvasElement) => {
+	const ctx = canvas.getContext('2d');
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
 

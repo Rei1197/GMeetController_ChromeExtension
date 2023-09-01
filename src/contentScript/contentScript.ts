67c6e04fa32a4e3a50d79be602f00015d03194e6
Rei-kun <reikamiya2016@gmail.com>
2023-09-01 21:31:51 +0900

    restart

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -23,8 +23,61 @@ chrome.runtime.onMessage.addListener(async (message: any, sender, sendResponse) 
       }
   }});
 
-  const clearCanvas = (canvas: HTMLCanvasElement) => {
+
+const clearCanvas = (canvas: HTMLCanvasElement) => {
 	const ctx = canvas.getContext('2d');
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
+const findVideo = () => {
+    const videoClass = document.getElementsByClassName("p2hjYe TPpRNe");
+    const video: HTMLVideoElement[] = [];
+
+    for (const container of Array.from(videoClass)){
+        const videos = container.getElementsByTagName("video");
+        if (videos.length > 0) {
+            video.push(videos[0]);
+        }
+    }
+
+    // //------------
+    // // for multiple videos
+    // for (const videoElement of videos) {
+    //     video.push(videoElement);
+    // }
+    // //------------
+    
+    return video;
+}
+
+const loadModel = async () => {
+
+    // find the video on google meet
+    const meetVideo = findVideo();
+    console.log(`Number of video elements: ${meetVideo.length}`);
+    if (meetVideo.length == 0) {
+        console.error("No video elements found on the page.");
+        return;
+    }
+
+    // loading the custom model
+    console.log('loading model...');
+    try {
+        await tf.setBackend('webgl');
+    if (tf.ready()){
+        const model = await loadGraphModel(customModelURL)
+        console.log("Model loaded successfully");
+    } else {
+        console.error ('TensorFlow.js backend is not ready.');
+        return;
+    }
+    } catch (error) {
+        console.error("Failed to load the model: ", error);
+        return;
+    }
+};
+
+const detection = () => {
+
+    console.log("you have reached to detection function");
+}

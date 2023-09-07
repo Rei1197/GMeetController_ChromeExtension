d59e99d329a79a0918ab32a575c666d0fa4d207a
Rei-kun <reikamiya2016@gmail.com>
2023-09-07 19:08:52 +0900

    update contentScript

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -3,12 +3,17 @@ import { loadGraphModel } from "@tensorflow/tfjs-converter";
 
 let isDetecting = false
 let customModelURL = chrome.runtime.getURL('best_web_model.json')
+let model: tf.GraphModel | null = null;
 
 chrome.runtime.onMessage.addListener(async (message: any, sender, sendResponse) => {
     if (message.type === 'startDetecting') {
       isDetecting = true;
-      await loadModel();
-      if (customModelURL) {
+      const loadedModel = await loadModel();
+      if (loadedModel) {
+
+        console.log("Model input shape:", loadedModel.inputs[0].shape);
+        console.log("Model output shape:", loadedModel.outputs);
+
         detection();
       }
     }
@@ -65,8 +70,9 @@ const loadModel = async () => {
     try {
         await tf.setBackend('webgl');
     if (tf.ready()){
-        const model = await loadGraphModel(customModelURL)
+        model = await loadGraphModel(customModelURL)
         console.log("Model loaded successfully");
+        return model; 
     } else {
         console.error ('TensorFlow.js backend is not ready.');
         return;
@@ -79,5 +85,39 @@ const loadModel = async () => {
 
 const detection = () => {
 
-    console.log("you have reached to detection function");
+    if (!model){
+        console.error("Model is not loaded yet.");
+        return;
+    }
+
+    const meetVideos = findVideo();
+    if (!isDetecting) return;
+
+    for (const meetVideo of meetVideos) {
+        // const canvas = document.createElement("canvas");
+        // canvas.width = meetVideo.videoWidth;
+        // canvas.height = meetVideo.height;
+        // const ctx = canvas.getContext("2d");
+        // // ctx.drawImage(meetVideo[0],0,0,meetVideo[0].videoWidth,meetVideo[0].videoHeight);
+        // ctx.drawImage(meetVideo, 0, 0, 640, 640);
+
+        const tfImage = tf.browser.fromPixels(meetVideo);
+        const resizedImage = tf.image.resizeBilinear(tfImage, [640, 640]); // assuming the model expects [224, 224, 3] input shape
+        const batchedImage = resizedImage.expandDims(0);
+        const preprocessedImage = batchedImage.toFloat().div(tf.scalar(255)); // normalizing
+
+        // Run the preprocessed frame through the model
+        const predictions = model.predict(preprocessedImage) as tf.Tensor;
+        predictions.print();
+        console.log("results: ", predictions);
+    
+        // Cleaning up tensors
+        tfImage.dispose();
+        resizedImage.dispose();
+        batchedImage.dispose();
+        preprocessedImage.dispose();
+
+        // Schedule the next frame processing
+        requestAnimationFrame(detection);
+    };
 }
